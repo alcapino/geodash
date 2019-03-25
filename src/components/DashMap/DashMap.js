0c6a8a534ad53b4a0f406c1f0f185f311471a40d
Paolo Vacarizas <vacarizas@vacarizas-macbookpro.roam.corp.google.com>
2019-03-25 19:06:47 +0800

    ADD map, render markers, WIP set up hooks and context

--- src/components/DashMap/DashMap.js
+++ src/components/DashMap/DashMap.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import {
-  withGoogleMap,
   GoogleMap,
   Marker,
+  withGoogleMap,
   withScriptjs
 } from "react-google-maps";
+import {withGeoContext} from '../../GeoContext/withGeoContext';
 
-const DashMap = withScriptjs(withGoogleMap(props =>
-  <GoogleMap
-    defaultZoom={5}
-    defaultCenter={{ lat: 12.8797207, lng: 121.7740173 }}
-    height={'100%'}
-  >
-  </GoogleMap>
-));
+const DashMap = withScriptjs(withGoogleMap(props =>{
+  const markers = props.contextData.geopoints.map(
+    (point, i) => <Marker key={i} position={{lat: point[1],lng: point[0]}} />
+  );
+  return(
+    <GoogleMap
+      defaultZoom={6}
+      defaultCenter={{ lat: 12.8797207, lng: 121.7740173 }}
+      height={'100%'}
+    >
+      {markers}
+    </GoogleMap>
+  );
+}));
 
-
-export default DashMap;
+export default withGeoContext(DashMap);
